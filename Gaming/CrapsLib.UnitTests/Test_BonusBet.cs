e5f4568bfdca598cbd80d7a4be021a3046cff450
tzerb <tzerb@hotmail.com>
2015-12-06 19:36:05 -0600

    Flesched out BounsBets

---
+++ Gaming/CrapsLib.UnitTests/Test_BonusBet.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrapsLib.UnitTests
+{
+    [TestClass]
+    public class Test_BonusBet
+    {
+        [TestMethod]
+        public void Test_SmallBonusBet_BasicWin()
+        {
+            decimal payout;
+            var smb = new SmallBonusBet(100);
+            Assert.IsTrue(smb.Roll(2, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(smb.Roll(3, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(smb.Roll(4, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(smb.Roll(5, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsFalse(smb.Roll(6, out payout));
+            Assert.AreEqual(payout, 3400);
+        }
+
+        [TestMethod]
+        public void Test_SmallBonusBet_BasicLoss()
+        {
+            decimal payout;
+            var smb = new SmallBonusBet(100);
+            Assert.IsFalse(smb.Roll(7, out payout));
+            Assert.AreEqual(payout, -100);
+        }
+
+        [TestMethod]
+        public void Test_TallBonusBet_BasicWin()
+        {
+            decimal payout;
+            var tbb = new TallBonusBet(100);
+            Assert.IsTrue(tbb.Roll(12, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(tbb.Roll(11, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(tbb.Roll(10, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(tbb.Roll(9, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsFalse(tbb.Roll(8, out payout));
+            Assert.AreEqual(payout, 3400);
+        }
+
+        [TestMethod]
+        public void Test_TallBonusBet_BasicLoss()
+        {
+            decimal payout;
+            var tbb = new TallBonusBet(100);
+            Assert.IsFalse(tbb.Roll(7, out payout));
+            Assert.AreEqual(payout, -100);
+        }
+
+        [TestMethod]
+        public void Test_AllBonusBet_BasicWin()
+        {
+            decimal payout;
+            var abb = new AllBonusBet(100);
+            Assert.IsTrue(abb.Roll(2, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(3, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(4, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(5, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(6, out payout));
+            Assert.AreEqual(payout, 0);
+
+            Assert.IsTrue(abb.Roll(8, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(9, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(10, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsTrue(abb.Roll(11, out payout));
+            Assert.AreEqual(payout, 0);
+            Assert.IsFalse(abb.Roll(12, out payout));
+
+            Assert.AreEqual(payout, 17500);
+        }
+        [TestMethod]
+        public void Test_AllBonusBet_BasicLoss()
+        {
+            decimal payout;
+            var abb = new AllBonusBet(100);
+            Assert.IsFalse(abb.Roll(7, out payout));
+            Assert.AreEqual(payout, -100);
+        }
+    }
+}
--- Gaming/CrapsLib/BonusBet.cs
+++ Gaming/CrapsLib/BonusBet.cs
@@ -6,7 +6,67 @@ using System.Threading.Tasks;
 
 namespace CrapsLib
 {
-    class BonusBet
+    public class BonusBet
     {
+        public decimal Amount { get; set; }
+        public decimal Multiplier { get; set; }
+
+        protected List<int> Numbers
+        {
+            get; set;
+        }
+        public BonusBet(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public bool Roll(int diceRoll, out decimal payout)
+        {
+            if (diceRoll == 7)
+            {
+                payout = -1M * Amount;
+                return false;
+            }
+
+            if (Numbers.Exists(d => d == diceRoll))
+                Numbers.Remove(diceRoll);
+
+            if (Numbers.Count == 0)
+            {
+                payout = Multiplier * Amount;
+                return false;
+            }
+            payout = 0M;
+            return true;
+        }
     }
+
+    public class TallBonusBet : BonusBet
+    {
+        public TallBonusBet(decimal amount) : base(amount)
+        {
+            Numbers = new List<int> { 12, 11, 10, 9, 8 };
+            Multiplier = 34M;
+        }
+    }
+
+    public class SmallBonusBet : BonusBet
+    {
+        public SmallBonusBet(decimal amount) : base(amount)
+        {
+            Numbers = new List<int> { 2, 3, 4, 5, 6 };
+            Multiplier = 34M;
+        }
+    }
+
+    public class AllBonusBet : BonusBet
+    {
+        public AllBonusBet(decimal amount) : base(amount)
+        {
+            Numbers = new List<int> { 12, 11, 10, 9, 8, 2, 3, 4, 5, 6 };
+            Multiplier = 175M;
+        }
+    }
+
+
 }
