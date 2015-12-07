4acfd5e84093d610a3eabc67d302691fbfe140d3
tzerb <tzerb@hotmail.com>
2015-12-06 19:51:23 -0600

    Fleshed out FieldBet.

---
+++ Gaming/CrapsLib.UnitTests/Test_FieldBet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrapsLib.UnitTests
+{
+    [TestClass]
+    public class Test_FieldBet
+    {
+        [TestMethod]
+        public void Test_SinglePayout()
+        {
+            decimal payout;
+            var fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(3, false, out payout));
+            Assert.AreEqual(payout, 100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(4, false, out payout));
+            Assert.AreEqual(payout, 100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(9, false, out payout));
+            Assert.AreEqual(payout, 100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(10, false, out payout));
+            Assert.AreEqual(payout, 100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(11, false, out payout));
+            Assert.AreEqual(payout, 100);
+
+        }
+
+        [TestMethod]
+        public void Test_DoublePayout()
+        {
+            decimal payout;
+            var fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(2, false, out payout));
+            Assert.AreEqual(payout, 200);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(12, false, out payout));
+            Assert.AreEqual(payout, 200);
+        }
+
+        [TestMethod]
+        public void Test_Loss()
+        {
+            decimal payout;
+            var fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(5, false, out payout));
+            Assert.AreEqual(payout, -100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(6, false, out payout));
+            Assert.AreEqual(payout, -100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(7, false, out payout));
+            Assert.AreEqual(payout, -100);
+
+            fb = new FieldBet(100);
+            Assert.IsFalse(fb.Roll(8, false, out payout));
+            Assert.AreEqual(payout, -100);
+        }
+
+    }
+}
--- Gaming/CrapsLib/FieldBet.cs
+++ Gaming/CrapsLib/FieldBet.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace CrapsLib
 {
-    class FieldBet
+    public class FieldBet
     {
+        public decimal Amount { get; set; }
+        public List<int> FieldNumbersSingle { get; set; }
+        public List<int> FieldNumbersDouble { get; set; }
+
+        public FieldBet(decimal amount)
+        {
+            FieldNumbersDouble = new List<int> { 2, 12 };
+            FieldNumbersSingle = new List<int> { 3, 4, 9, 10, 11};
+            Amount = amount;
+        }
+
+        public bool Roll(int diceRoll, bool hard, out decimal payout)
+        {
+            if (FieldNumbersDouble.Exists(d => d == diceRoll))
+            {
+                payout = 2M * Amount;
+            }
+            else if (FieldNumbersSingle.Exists(d => d == diceRoll))
+            {
+                payout = 1M * Amount;
+            }
+            else
+                payout = -1M * Amount;
+
+            return false;
+        }
     }
 }
