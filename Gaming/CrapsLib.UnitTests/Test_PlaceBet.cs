f20b1af223a24469f4869edf10340d1c04615735
tzerb <tzerb@hotmail.com>
2015-12-06 20:13:40 -0600

    Fleshed out PlaceBet

---
+++ Gaming/CrapsLib.UnitTests/Test_PlaceBet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrapsLib.Bets;
+
+namespace CrapsLib.UnitTests
+{
+    [TestClass]
+    public class Test_PlaceBet
+    {
+        [TestMethod]
+        public void Test_6_Win()
+        {
+            decimal payout;
+            var pb = new PlaceBet(120, 6);
+            Assert.IsFalse(pb.Roll(6, out payout));
+            Assert.AreEqual(payout, 140);
+        }
+        [TestMethod]
+        public void Test_6_Loss()
+        {
+            decimal payout;
+            var pb = new PlaceBet(120, 6);
+            Assert.IsFalse(pb.Roll(7, out payout));
+            Assert.AreEqual(payout, -120);
+        }
+    }
+}
--- Gaming/CrapsLib/Bets/PlaceBet.cs
+++ Gaming/CrapsLib/Bets/PlaceBet.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace CrapsLib.Bets
 {
-    class PlaceBet
+    public class PlaceBet
     {
+        public decimal Amount { get; }
+        public int DesiredRoll { get; }
+        private Dictionary<int, decimal> multipliers = new Dictionary<int, decimal>();
+        public PlaceBet(decimal amount, int desiredRoll)
+        {
+            Amount = amount;
+            DesiredRoll = desiredRoll;
+            multipliers.Add(4, 9M / 5M);
+            multipliers.Add(5, 7M / 5M);
+            multipliers.Add(6, 7M / 6M);
+            multipliers.Add(8, 7M / 6M);
+            multipliers.Add(9, 7M / 5M);
+            multipliers.Add(10, 9M / 5M);
+        }
+
+        public bool Roll(int diceRoll, out decimal payout)
+        {
+            if (diceRoll == 7)
+            {
+                payout = -1M * Amount;
+                return false;
+            }
+
+            if (multipliers.ContainsKey(diceRoll))
+            {
+                payout = multipliers[diceRoll] * Amount;
+                return false;
+            }
+
+            payout = 0;
+            return true;
+        }
     }
 }
